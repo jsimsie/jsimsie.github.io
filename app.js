3f6c1563d5b6e9eb77685166056ec5c518740fa9
jsimsie <132791822+jsimsie@users.noreply.github.com>
2023-05-07 12:27:29 +1200

    Updated Code

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-window.addEventListener("load", function() {
-  document.querySelector("#loader-wrapper").classList.add("loaded");
+// Smooth scrolling for anchor links
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
 });
+
+// Navigation menu toggle
+$('.menu-toggle').click(function() {
+$('.nav-links').toggleClass('active');
+});
+
+// Sticky navigation bar
+window.onscroll = function() {stickyNav()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNav() {
+if (window.pageYOffset >= sticky) {
+  navbar.classList.add("sticky");
+} else {
+  navbar.classList.remove("sticky");
+}
+}
